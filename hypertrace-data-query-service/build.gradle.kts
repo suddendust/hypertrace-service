240b15eca0e3f4ae332b45ccf52370643ff39af2
Prashant Pandey <prashant.pandey@razorpay.com>
2022-04-25 18:46:45 +0530

    Correct dockerfile path

--- hypertrace-data-query-service/build.gradle.kts
+++ hypertrace-data-query-service/build.gradle.kts
@@ -41,13 +41,14 @@ application {
 
 hypertraceDocker {
   image("hypertrace-data-query-service-custom-image-2") {
-    dockerFile.set(file("build/docker/Dockerfile"))
+    dockerFile.set(file("../hypertrace-service/Dockerfile"))
+    buildArgs.put("HYPERTRACE_UI_VERSION", "latest")
     javaApplication {
       envVars.put("CLUSTER_NAME", "default-cluster")
       envVars.put("POD_NAME", "default-pod")
     }
   }
-  tag("custom-tag-2") {
+  tag("custom-tag-3") {
     onlyIf { candidateImage ->
       candidateImage.name == "hypertrace-data-query-service-custom-image-2"
     }
